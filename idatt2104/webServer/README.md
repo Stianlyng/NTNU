f5c76c6c9eb2a745d5876ba375707b87e850c0f9
Stian Lyng <stianlyng@protonamail.com>
2023-02-15 16:29:02 +0100

    finised implemening threads

---
+++ idatt2104/webServer/README.md
@@ -0,0 +1,9 @@
+## A Multithreaded Socket Server 
+
+That can calculate input from multiple clients, running in their own thread
+
+Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+
+## Based on examplecode from lecture
+
+Implemented a Runnable in the SocketTjener Example. And kept SocketKlient as is.
--- idatt2104/webServer/src/SocketTjener.java
+++ idatt2104/webServer/src/SocketTjener.java
@@ -1,48 +1,59 @@
-/**
- * SocketTjener.java  - "Programmering i Java", 4.utgave - 2009-07-01
- *
- * Programmet åpner en socket og venter på at en klient skal ta kontakt.
- * Programmet leser tekster som klienten sender over, og returnerer disse.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
- import java.io.*;
- import java.net.*;
- 
- class SocketTjener {
-   public static void main(String[] args) throws IOException {
-     final int PORTNR = 1250;
- 
-     ServerSocket tjener = new ServerSocket(PORTNR);
-     System.out.println("Logg for tjenersiden. Nå venter vi...");
-     Socket forbindelse = tjener.accept();  // venter inntil noen tar kontakt
- 
-     /* Åpner strømmer for kommunikasjon med klientprogrammet */
-     InputStreamReader leseforbindelse
-       = new InputStreamReader(forbindelse.getInputStream());
-     BufferedReader leseren = new BufferedReader(leseforbindelse);
-     PrintWriter skriveren = new PrintWriter(forbindelse.getOutputStream(), true);
- 
-     /* Sender innledning til klienten */
-     skriveren.println("Hei, du har kontakt med tjenersiden!");
-     skriveren.println("Skriv hva du vil, så skal jeg gjenta det, avslutt med linjeskift.");
- 
-     /* Mottar data fra klienten */
-     String enLinje = leseren.readLine();  // mottar en linje med tekst
-     while (enLinje != null) {  // forbindelsen på klientsiden er lukket
-       System.out.println("En klient skrev: " + enLinje);
-       skriveren.println("Du skrev: " + enLinje);  // sender svar til klienten
-       enLinje = leseren.readLine();
-     }
- 
-     /* Lukker forbindelsen */
-     leseren.close();
-     skriveren.close();
-     forbindelse.close();
-   }
- }
- 
- /* Utskrift på tjenersiden:
- Logg for tjenersiden. Nå venter vi...
- En klient skrev: Hallo, dette er en prøve.
- En klient skrev: Og det fungerer utmerket.
- */
+public class SocketTjener implements Runnable {
+
+  private Socket socket; // denne variabelen brukes i run-metoden
+
+  public SocketTjener(Socket socket) {
+    this.socket = socket; // konstruktøren tar imot socket som parameter
+  }
+
+  @Override
+  public void run() {
+    try {
+      // Åpner strømmer for kommunikasjon med klientprogrammet
+      BufferedReader leseren = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      PrintWriter skriveren = new PrintWriter(socket.getOutputStream(), true);
+      
+      // Sender innledning til klienten
+      skriveren.println("Hei, du har kontakt med tjenersiden!");
+      skriveren.println("Skriv hva du vil, så skal jeg gjenta det, avslutt med linjeskift.");
+      
+      // Mottar data fra klienten
+      String enLinje = leseren.readLine(); // mottar en linje med tekst
+      while (enLinje != null) { // forbindelsen på klientsiden er lukket
+        System.out.println("En klient skrev: " + enLinje);
+        skriveren.println("Du skrev: " + enLinje); // sender svar til klienten
+        enLinje = leseren.readLine();
+      }
+      
+      // Lukker forbindelsen
+      leseren.close();
+      skriveren.close();
+      socket.close(); // husk å lukke socketen
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    final int PORTNR = 1250;
+    ServerSocket tjener = new ServerSocket(PORTNR);
+    System.out.println("Logg for tjenersiden. Nå venter vi...");
+    
+    while (true) {
+      Socket socket = tjener.accept(); // venter inntil noen tar kontakt
+      System.out.println("En klient har koblet til.");
+      // Opprett en ny tråd for hver ny forbindelse
+      Thread t = new Thread(new SocketTjener(socket));
+      t.start();
+    }
+    
+    
+  }
+}
